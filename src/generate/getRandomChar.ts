c9e4bcfa442b6ba0fb5f47d2307677db031c78d1
Riyas Iqbal <riyasiqbal10@gmail.com>
2023-06-26 14:44:51 +0530

    refractor: clean code + JsDoc

--- src/generate/getRandomChar.ts
+++ src/generate/getRandomChar.ts
@@ -6,21 +6,20 @@
  * @returns {string} The randomly generated character.
  */
 export function getRandomChar(min: number, max: number): string {
-    const range = max - min + 1;
-    const randomIndex = Math.floor(Math.random() * range);
-    const randomChar = String.fromCharCode(randomIndex + min);
+    const range = max - min + 1; //total number of characters in the specified range
+    const randomIndex = Math.floor(Math.random() * range);  //get random index between the range
+    const randomChar = String.fromCharCode(randomIndex + min); // add minimum value to randomIndex generated
     return randomChar;
 }
 
 /**
  * Generates a random special character from the predefined list of special characters.
- * 
+ * list = !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~'
  * @returns {string} The randomly generated special character.
  */
 export function getSpecialChar() {
     const specialChar = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~'";
     const randomIndex = Math.floor(Math.random() * specialChar.length);
     const randomChar = specialChar[randomIndex];
-
     return randomChar;
 }
